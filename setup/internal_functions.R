6c582e908dc8a932b7656a151739dc8412d1a0dd
Vegard Lysne <vegard.lysne@helsedir.no>
2024-09-13 11:03:20 +0200

    add kvalitetskontrollrutiner

--- setup/internal_functions.R
+++ setup/internal_functions.R
@@ -1,17 +1,7 @@
 use_khfunctions <- function(){
   message("Loading KHfunctions\n---\n")
-  message("Currentlynot available")
-  # source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHsetup.R")
-}
-
-use_orgdata <- function(){
-  message("Loading orgdata\n---\n")
-  library(orgdata)
-}
-
-use_qualcontrol <- function(){
-  message("Loading KHvalitetskontroll\n---\n")
   message("Currently not available")
+  # source("https://raw.githubusercontent.com/helseprofil/khfunctions/master/R/KHsetup.R")
 }
 
 is_updates <- function(){
