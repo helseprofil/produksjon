41414791c930d2ee0d59503a2deb7f55e404e6c8
Vegard Lysne <vegard.lysne@helsedir.no>
2024-09-13 13:44:42 +0200

    edit welcome

--- setup/welcome.R
+++ setup/welcome.R
@@ -1,7 +1,8 @@
 message("Velkommen til Produksjonsløypen!\n",
         "---\n",
-        "- For å bruke orgdata, bruk `use_orgdata()`\n",
-        "- For å bruke khfunctions, bruk `use_khfunctions()`\n",
-        "- For å bruke KHvalitetskontroll, bruk `use_qualcontrol()`\n",
+        "Brukerfiler med kode ligger i de respektive undermappene\n",
         "---\n",
-        "Brukerfiler med kode ligger i de respektive undermappene")
+        "- For å bruke orgdata, bruk `library(orgdata)`\n",
+        # "- For å bruke khfunctions, bruk `use_khfunctions()`\n",
+        "- For å bruke kvalitetskontroll, bruk `library(qualcontrol)`\n",
+        "---\n")
