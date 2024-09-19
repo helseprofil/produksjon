1daad5e22962ddf257ecd8fc182d071b58d664bb
Vegard Lysne <vegard.lysne@helsedir.no>
2024-09-19 09:07:34 +0200

    update user files and readme, add update_packages()

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Masterprosjekt med brukerfiler for alle delene av produksjonsapparatet knyttet til produksjon av folkehelse- og oppvekstprofiler. 
 
+Ved oppstart vil du få beskjed om det er kommet endringer i brukerfilene, og mulighet for å oppdatere til siste versjon. 
+
+For å oppdatere pakker (orgdata, norgeo, qualcontrol og andre pakker fra CRAN), bruk funksjonen `update_packages()`
+
+
+--- 
+
 Relaterte prosjekter: 
 - **Misc**:
   - Diverse funksjoner som ikke passer inn i andre prosjekter
@@ -22,9 +29,9 @@ Relaterte prosjekter:
 - **orgdata**
   - Lesing og aggregering av originale datafiler
   
-- **khfunctions** (etter hvert orgcube)
+- **khfunctions** (etter hvert "orgcube")
   - Prosessering av originale og aggregerte data til filgruppe og kube/friskvikfil
   
-- **KHvalitetskontroll** (etter hvert qualcontrol)
+- **qualcontrol** (tidligere KHvalitetskontroll)
   - Kvalitetskontroll av ferdige datafiler før publisering
   
--- setup/internal_functions.R
+++ setup/internal_functions.R
@@ -5,7 +5,7 @@
 # }
 
 is_updates <- function(){
-  currentversion <- "2024.09.13" # Update this whenever updating user files
+  currentversion <- "2024.09.19" # Update this whenever updating user files
   localversion <- character()
   if(file.exists("setup/internal_functions.R")){
     localversion <- sub(".*\"(.*)\".*", "\\1", grep("currentversion <-", readLines("setup/internal_functions.R"), value = T)[1])
@@ -14,7 +14,6 @@ is_updates <- function(){
 }
 
 update_userfiles <- function(){
-
   choice <- utils::menu(choices = c("Yes", "No"),
                         title = paste0("\nOppdaterte brukerfiler er tilgjengelige!!",
                                        "\n\nOppdater (anbefalt)?"))
@@ -27,4 +26,12 @@ update_userfiles <- function(){
   } else {
     message("\nSkipper oppdateringer, brukerfilene kan være utdaterte.")
   }
+}
+
+#' @title update_packages
+#' @description Updates packages from cran and GitHub
+update_packages <- function(){
+  source("https://raw.githubusercontent.com/helseprofil/misc/main/ProfileSystems.R")
+  ProfileSystems(all = F, packages = T, norgeo = T, orgdata = T, qualcontrol = T)
+  rm(ProfileSystems, DevelopSystems, check_R_version, envir = .GlobalEnv)
 }
