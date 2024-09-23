4f56267ea04bb48fc74feea363392bf904b7f91b
Vegard Lysne <vegard.lysne@helsedir.no>
2024-09-23 15:35:43 +0200

    updates

--- setup/setup.R
+++ setup/setup.R
@@ -1,7 +1,7 @@
 Sys.setlocale("LC_ALL", "nb-NO.UTF-8")
 source("https://raw.githubusercontent.com/helseprofil/produksjon/main/setup/internal_functions.R")
 
-lastupdated <- "2024.09.20"
+lastupdated <- "2024.09.23"
 if(is_updates(lastupdated)){
   update_userfiles()
 }
--- setup/welcome.R
+++ setup/welcome.R
@@ -1,8 +1,8 @@
 message("Velkommen til Produksjonsløypen!\n",
         "---\n",
-        "Brukerfiler med kode ligger i de respektive undermappene\n",
-        "---\n",
-        "- For å bruke orgdata, bruk `library(orgdata)`\n",
+        "Brukerfiler med kode ligger i de respektive undermappen: e\n",
+        "- For å bruke orgdata, bruk filen `orgdata brukerkode`\n",
         # "- For å bruke khfunctions, bruk `use_khfunctions()`\n",
-        "- For å bruke kvalitetskontroll, bruk `library(qualcontrol)`\n",
-        "---\n")
+        "- For å bruke kvalitetskontroll, bruk dokumentet `Kvalitetskontrollrutiner`\n",
+        "---\n",
+        "For å oppdatere alle pakker, bruk funksjonen `update_packages()`")
