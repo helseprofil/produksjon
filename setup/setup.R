4a626b43d7ae2c6e0a073a55caac798d1589e84b
Vegard Lysne <vegard.lysne@helsedir.no>
2024-09-24 10:02:20 +0200

    update qualcontrol folder

--- setup/setup.R
+++ setup/setup.R
@@ -1,7 +1,7 @@
 Sys.setlocale("LC_ALL", "nb-NO.UTF-8")
 source("https://raw.githubusercontent.com/helseprofil/produksjon/main/setup/internal_functions.R")
 
-lastupdated <- "2024.09.23"
+lastupdated <- "2024.09.24"
 if(is_updates(lastupdated)){
   update_userfiles()
 }
