8b51871e0d0fdb059238adaccb6abb69bfdbf2f3
Vegard <vegard.lysne@helsedir.no>
2025-01-30 14:47:03 +0100

    update user files

--- setup/welcome.R
+++ setup/welcome.R
@@ -1,8 +1,8 @@
 message("Velkommen til Produksjonsløypen!\n",
         "---\n",
-        "Brukerfiler med kode ligger i de respektive undermappen: e\n",
+        "Brukerfiler med kode ligger i de respektive undermappene:\n",
         "- For å bruke orgdata, bruk filen `orgdata brukerkode.Rmd`\n",
         "- For å bruke khfunctions, bruk filen `khfunctions brukerkode.Rmd`\n",
         "- For å bruke kvalitetskontroll, bruk dokumentet `Kvalitetskontrollrutiner.Rmd`\n",
         "---\n",
-        "For å oppdatere alle pakker, bruk funksjonen `update_packages()`")
+        "For å oppdatere alle pakker og prosjekter, bruk funksjonen `update_packages()`")
