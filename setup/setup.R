5e90c567979be8499ae754c180f40b9fa867e6ba
Vegard <vegard.lysne@helsedir.no>
2025-06-13 11:26:52 +0200

    update user files

--- setup/setup.R
+++ setup/setup.R
@@ -1,7 +1,7 @@
 Sys.setlocale("LC_ALL", "nb-NO.UTF-8")
 source("https://raw.githubusercontent.com/helseprofil/produksjon/main/setup/internal_functions.R")
 
-lastupdated <- "2025.03.10"
+lastupdated <- "2025.06.13"
 if(is_updates(lastupdated)){
   update_userfiles()
 }
