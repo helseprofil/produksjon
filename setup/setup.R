b07fe1f5f88c2a1dc67b7f93c0aebe3dee8718f9
Vegard <vegard.lysne@helsedir.no>
2025-08-11 11:46:17 +0200

    remove internal dim/val-lists

--- setup/setup.R
+++ setup/setup.R
@@ -9,9 +9,6 @@ if(is_updates(lastupdated)){
 rm(lastupdated)
 rm(is_updates)
 
-DIMENSIONS_ALL <- qualcontrol:::.validdims
-DIMENSIONS_STANDARD <- qualcontrol:::.standarddimensions
-VALUES_STANDARD <- qualcontrol:::.standardvalues
 GEO_VALID <- qualcontrol:::.validgeo
 GEO_RECODE <- qualcontrol:::.georecode
 POPULATION_TABLE <- qualcontrol:::.popinfo
