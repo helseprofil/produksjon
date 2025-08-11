347d17dac639a7e38875669788fa51509eeac859
Vegard <vegard.lysne@helsedir.no>
2025-08-11 13:00:39 +0200

    small edit

--- setup/setup.R
+++ setup/setup.R
@@ -13,9 +13,9 @@ if(interactive()){
   GEO_VALID <- qualcontrol:::.validgeo
   GEO_RECODE <- qualcontrol:::.georecode
   POPULATION_TABLE <- qualcontrol:::.popinfo
-
-  updates <- look_for_new_versions()
 }
+
+updates <- look_for_new_versions()
 
 source("https://raw.githubusercontent.com/helseprofil/produksjon/main/setup/welcome.R")
 if(!is.null(updates)) message(updates)
