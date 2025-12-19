54871ee87747f660136836f942e11ed2fe7583a8
Vegard <vegard.lysne@helsedir.no>
2025-12-19 14:51:06 +0100

    small edit

--- setup/setup.R
+++ setup/setup.R
@@ -2,7 +2,7 @@ Sys.setlocale("LC_ALL", "nb-NO.UTF-8")
 source("https://raw.githubusercontent.com/helseprofil/produksjon/main/setup/internal_functions.R")
 options(warn = 1)
 
-lastupdated <- "2025-12-19b"
+lastupdated <- "2025-12-19c"
 if(interactive()){
   if(is_updates(lastupdated)){
     update_userfiles()
